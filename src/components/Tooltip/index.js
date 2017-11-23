258268aa21a7994294da342202da273450a8f33c
Giovanni Bernini <giovannibernini.ti@gmail.com>
2017-11-23 02:50:02 -0200

    propTypes added

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import classNames from 'classnames/bind'
 import style from './Tooltip.sass'
 
 let cx = classNames.bind(style)
 
-const Tooltip = ({ children, text, type = 'top' }) => {
+const Tooltip = ({ children, text, type }) => {
   const className = cx({
     tooltiptext: true,
     [`_${type}`]: true
@@ -16,6 +17,16 @@ const Tooltip = ({ children, text, type = 'top' }) => {
       <span className={className}>{text}</span>
     </div>
   )
+}
+
+Tooltip.defaultProps = {
+  type: 'top'
+}
+
+Tooltip.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired
 }
 
 export default Tooltip
